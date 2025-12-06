583aa1bac943e26a85358983c3b888339e04b3aa
Akmal Xayrullayev <Akmalweb.dev@gmail.com>
2025-12-06 12:52:16 +0500

    first commit

---
+++ 3dars/3dars/Program.cs
@@ -0,0 +1,170 @@
+﻿namespace _3dars
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.Write("birinchi sonni kiriting: ");
+            //int num1 = int.Parse(Console.ReadLine());
+            //Console.Write("ikkinchi sonni kiriting: ");
+            //int num2 = int.Parse(Console.ReadLine());
+
+            //for (int i = num1; i <= num2; i++)
+            //{
+            //    if (i % 3 == 0)
+            //        Console.Write($"{i}, ");
+
+
+            // 1.3.1 homework 
+
+            // 1 (osson)
+            //for (int i = 1; i <= 20; i++)
+            //{
+
+            //    Console.WriteLine(i);
+
+            //}
+
+            // 2 (osson)
+            //for (int i = 25; i <= 100; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // 3 (osson)
+            //for (int  i = 12;  i < 200;  i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+
+            // 4 (osson)
+            //for (int i = 15; i >= 2; i--)
+            //Console.WriteLine(i);
+
+            // 5 (osson)
+            //for (int i = 120; i >= 23; i--)
+            //{
+            //Console.WriteLine(i);
+            //}
+
+            // 6 (osson)
+            //for (int i=10; i <= 50; i++)
+            //{
+            //    if(i %2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            // 7 (osson)
+
+            //for (int i = 120; i <= 250; i++)
+            //{
+            //    if(i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+
+            //}
+
+            // 8 (osson)
+
+            //for (int i = 100; i >= 50; i--)
+            //{
+            //    if(i%2 != 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            // 9 (osson)
+
+            //for(int i = 80; i >= 20; i--)
+            //{
+            //    if (i%2==0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            // 10 (osson)
+
+            //Console.WriteLine("a ni kiriting: ");
+            //int a=int.Parse(Console.ReadLine());
+            //Console.WriteLine("b ni kiriting: ");
+            //int b=int.Parse(Console.ReadLine());
+
+            //for (int i=a; i <=b; i++)
+            //{
+            //    if (i %5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //11 (osson)
+            //Console.WriteLine("a ni kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("b ni kiriting: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if(i%7==0 && i % 4 == 0)
+            //    {
+            //        Console.WriteLine("4 ga ham 7 ga ham bo'linadi");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("4 ga ham 7 ga ham bo'linmaydi");
+            //    }
+            //}   // question!!!
+
+            //12 (osson)
+
+            //Console.WriteLine("a ni kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("b ni kiriting: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //int count = 0;
+
+            //for (int i = a; i <= b; i++)
+            //{
+            //    if (i % 8 == 0)
+            //    {
+            //        count++;
+            //    }
+            //}
+
+            //Console.WriteLine("8ga karrali sonlar soni: "+count);
+
+            //13 (osson)
+
+            //Console.WriteLine("a ni kiriting: ");
+            //int a = int.Parse(Console.ReadLine());
+            //Console.WriteLine("b ni kiriting: ");
+            //int b = int.Parse(Console.ReadLine());
+
+            //for (int i = b; i >= a; i--)
+            //{
+            //    if (i % 9 == 0 || i % 5 == 0)
+            //    {
+            //        Console.WriteLine(i);
+            //    }
+            //}
+
+            //14 (osson)
+
+
+            //int sum = 0;
+
+            //for (int i = 1; i <= 100; i += 2)
+            //{
+
+            //    sum += 1;
+            //}
+            //Console.WriteLine("1dan yuzgacha toq sonlar yig'indisi: "+sum);
+        }
+    }
+}
